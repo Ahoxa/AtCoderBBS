ee8bafebee7f50d6878eb2207f39f42f468a18c8
hayashibara <yujinyuto1223@gmail.com>
2023-12-04 17:57:24 +0900

    setting axios

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function index() {
   const [message, setMessage] = useState("Loading");
 
   useEffect(() => {
-    fetch("http://localhost:8080")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMessage(data.message);
+    axios
+      .get("http://localhost:8080")
+      .then((res) => {
+        console.log(res.data);
+        setMessage(res.data.message);
       })
       .catch((err) => console.log("Error: ", err));
   }, []);
